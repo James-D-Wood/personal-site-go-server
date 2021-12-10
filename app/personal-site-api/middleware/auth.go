7afeb4b2a16e50990a4dae7d21f266ffe7d0b2b6
James Wood <woodjames275@gmail.com>
2021-12-10 10:27:59 -0700

    feat: move auth key and psql config to env

--- app/personal-site-api/middleware/auth.go
+++ app/personal-site-api/middleware/auth.go
@@ -1,14 +1,21 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func AuthMiddleware(next http.Handler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// TODO: implement real auth
+		authKey := os.Getenv("PS_Auth_Key")
+		if authKey == "" {
+			http.Error(w, "Auth Error", http.StatusInternalServerError)
+			return
+		}
 		auth := r.Header.Get("Authorization")
-		if auth != "Bearer mysecret" {
+		if auth != fmt.Sprintf("Bearer %s", authKey) {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
